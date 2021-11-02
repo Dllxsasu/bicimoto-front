f5d9fe147875c2458c6bb578c4ba191f58bac61c
Your Name <sasudxd@example.com>
2021-11-02 10:42:23 -0500

    el cambio de

--- src/app/productsmain/productsmain.component.ts
+++ src/app/productsmain/productsmain.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-productsmain',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsmainComponent implements OnInit {
 
+  @Input()  nombre:string="name";
+  @Input() precio:string="price";
+  @Input() marca:string="marxk"; 
+  @Input() img:string="../../assets/prodct_img/aceite_quenda.png";
   constructor() { }
 
   ngOnInit(): void {
